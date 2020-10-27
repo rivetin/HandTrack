c4d2ac82b9186a649dc8f133ee77785e0ffa3b32
athulbabykurian <athulofficial01@gmail.com>
2020-10-27 21:11:17 +0530

    logging predictions some WebGl errors

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+//Default parameters for the script
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+  }
+  
+
 navigator.getUserMedia = navigator.getUserMedia ||
 navigator.webkitGetUserMedia ||
 navigator.mozGetUserMedia ||
@@ -11,6 +21,8 @@ const audio = document.querySelector("#audio");
 const canvas = document.querySelector("#canvas");
 const context = canvas.getContext("2d");
 
+
+
 let model;
 
 handTrack.startVideo(video)
@@ -19,14 +31,20 @@ handTrack.startVideo(video)
             {video: {}},
             stream => {
                 video.srcObject = stream;
+                setInterval(runDetection, 1000);
             },
             err => console.log(err)
         );
     });
 
+function runDetection(){
+    model.detect(video)
+        .then(predictions => {
+            console.log(predictions);
+        });
+}
 
-handTrack.load().then(
-    lmodel => {
+handTrack.load(modelParams).then(lmodel => {
         model = lmodel;
     }
 );
